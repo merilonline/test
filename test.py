5e25369a93c7c5478dc1ac80b981d7a7921214c5
meril abraham <merilonline@gmail.com>
2018-10-25 23:45:08 +0530

    first

---
+++ test.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello there"
+
+@app.route("fact")
+def rand():
+    foo = ['its beautiful', 'its hot', 'its good', 'its calm', 'its peaceful']
+    return random.choice(foo);
